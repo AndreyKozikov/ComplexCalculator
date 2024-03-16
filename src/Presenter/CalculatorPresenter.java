5dee2255fdf485fe845d84c140d24bf9e60536b9
Andrey Kozikov <nwisdom@mail.ru>
2024-03-16 22:08:10 +0500

    Second commit

--- src/Presenter/CalculatorPresenter.java
+++ src/Presenter/CalculatorPresenter.java
@@ -1,11 +1,11 @@
 package Presenter;
 
-import Module.ComplexOperations.*;
+import Module.ComplexNumber;
+import Module.ComplexOperations.OperationsList;
 import Module.ComplexOperations.impl.ComplexOperations;
 import Presenter.Util.CalculatorHistory;
 import Presenter.Util.Operations;
 import View.CalculatorView;
-import Module.ComplexNumber;
 
 
 public class CalculatorPresenter {
@@ -16,20 +16,31 @@ public class CalculatorPresenter {
         this.view = view;
         this.history = history;
     }
-    private void handleUserInput(){
+
+    private void handleUserInput() {
         Operations operationType = view.getOperationChoice();
         while (operationType != Operations.EXIT) {
             ComplexOperations operation = OperationsList.getOperation(operationType);
             System.out.println("Введите комплексные числа в формате a+bi, где a и b - вещественные числа.");
-            ComplexNumber number1 = view.getComplexNumberInput("Введите первое комплексное число: ");
-            ComplexNumber number2 = view.getComplexNumberInput("Введите второе комплексное число: ");
-            ComplexNumber result = operation.performOperation(number1, number2);
-            String his = "Операция: " + operationType + "\n" + number1.toString() + "\n" + number2.toString() + "\n" + "Результат: " + result.toString() + "\n";
-            history.addEntry(his);
-            view.displayResult(result);
+
+            try {
+                ComplexNumber number1 = view.getComplexNumberInput("Введите первое комплексное число: ");
+                ComplexNumber number2 = view.getComplexNumberInput("Введите второе комплексное число: ");
+                ComplexNumber result = operation.performOperation(number1, number2);
+                String his = "Операция: " + operationType + "\n" + number1.toString() + "\n" + number2.toString() + "\n" + "Результат: " + result.toString() + "\n";
+                history.addEntry(his);
+                view.displayResult(result);
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Повторите операцию с другими числами.");
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Повторите операцию с другими числами.");
+            }
             operationType = view.getOperationChoice();
         }
     }
+
     public void start() {
         handleUserInput();
     }
